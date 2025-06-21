822ce18a0ab9ea0c7ac46233744d771c073661f0
georgeherois <46408877+georgeherois@users.noreply.github.com>
2025-06-20 22:34:15 -0300

    Arquivos atualizado em 20/06/2025
    Arquivos atualizado em 20/06/2025

--- views/template.php
+++ views/template.php
@@ -87,18 +87,19 @@
           <div class="col-auto menu">
             <a href="<?php echo BASE_URL; ?>licitante"><img src="<?php echo BASE_URL; ?>assets/img/logo-card.png" class="rounded mx-auto d-block" alt="..."></a>
             <br /><br />
-            <form action="<?php echo BASE_URL; ?>api/consulta" method="post">
+            <form action="<?php echo BASE_URL; ?>api/consultar" method="post">
               <div class="form-group">
                 <label for="formGroupExampleInput">Fonte</label>
                 <select name="fonte" id="selecionar" class="form-control fill">
-                  <option value="">Selecione...</option>
+                  <option value="4" <?php echo (isset($_POST['fonte']) && $_POST['fonte'] == '4') ? 'selected' : ''; ?>>Selecione...</option>
                   <option value="3" <?php echo (isset($_POST['fonte']) && $_POST['fonte'] == '3') ? 'selected' : ''; ?>>Compras Gov</option>
                   <option data-section="2" value="2" <?php echo (isset($_POST['fonte']) && $_POST['fonte'] == '2') ? 'selected' : ''; ?>>Compras Publicas</option>
                   <option value="1" <?php echo (isset($_POST['fonte']) && $_POST['fonte'] == '1') ? 'selected' : ''; ?>>Outros</option>
                 </select>
+                <br />
               </div>
-              <br />
-              <div data-name="2">
+
+              <div data-name="2" id="status-group-govA">
                 <div class="form-group">
                   <label for="formGroupExampleInput2">Tipo</label>
                   <select name="tipo" class="form-control fill">
@@ -109,7 +110,7 @@
                 </div>
               </div>
               <br />
-              <div data-name="2">
+              <div data-name="2" id="status-group-govB">
                 <div class="form-group">
                   <label for="formGroupExampleInput2">Data Inicial</label>
                   <input type="date" name="dataInicial" class="form-control" value="<?php echo isset($_POST['dataInicial']) ? $_POST['dataInicial'] : ''; ?>">
@@ -129,13 +130,47 @@
                     <option value="0" <?php echo (isset($_POST['status']) && $_POST['status'] == '0') ? 'selected' : ''; ?>>Status</option>
                     <option value="1" <?php echo (isset($_POST['status']) && $_POST['status'] == '1') ? 'selected' : ''; ?>>Recebendo Propostas</option>
                     <option value="2" <?php echo (isset($_POST['status']) && $_POST['status'] == '2') ? 'selected' : ''; ?>>Em Andamento</option>
+                    <option value="25" <?php echo (isset($_POST['status']) && $_POST['status'] == '25') ? 'selected' : ''; ?>>Em Republicação</option>
                     <option value="3" <?php echo (isset($_POST['status']) && $_POST['status'] == '3') ? 'selected' : ''; ?>>Finalizado</option>
                     <option value="4" <?php echo (isset($_POST['status']) && $_POST['status'] == '4') ? 'selected' : ''; ?>>Iminência de deserto</option>
                   </select>
                 </div>
+
+                <br />
+
+
+                <div class="form-group">
+                  <label for="formGroupExampleInput2">Modo Compras Publicas</label>
+                  <select name="modoModalidade" id="selecionar" class="form-control fill">
+                    <option value="">Selecione...</option>
+                    <option value="10" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '10') ? 'selected' : ''; ?>>Chamada Pública Agricultura</option>
+                    <option value="11" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '11') ? 'selected' : ''; ?>>Chamamento Publica - 13.019</option>
+                    <option value="19" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '19') ? 'selected' : ''; ?>>Chamamento Publico - 9.637</option>
+                    <option value="6" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '6') ? 'selected' : ''; ?>>Concorrência</option>
+                    <option value="14" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '14') ? 'selected' : ''; ?>>Concurso</option>
+                    <option value="4" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '4') ? 'selected' : ''; ?>>Cotação</option>
+                    <option value="8" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '8') ? 'selected' : ''; ?>>Contratação Direta</option>
+                    <option value="7" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '7') ? 'selected' : ''; ?>>Credenciamento</option>
+                    <option value="3" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '3') ? 'selected' : ''; ?>>Dispensa</option>
+                    <option value="92" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '92') ? 'selected' : ''; ?>>Dispensa - Lei das Estatais 13.303</option>
+                    <option value="13" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '13') ? 'selected' : ''; ?>>Inexigibilidade</option>
+                    <option value="16" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '16') ? 'selected' : ''; ?>>IRP-Intenção para Regsitro de Preço</option>
+                    <option value="5" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '5') ? 'selected' : ''; ?>>Leilão</option>
+                    <option value="12" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '12') ? 'selected' : ''; ?>>Leilão Eletrônico</option>
+                    <option value="15" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '15') ? 'selected' : ''; ?>>Pré-Qualificação</option>
+                    <option value="1" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '1') ? 'selected' : ''; ?>>Pregão</option>
+                    <option value="91" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '91') ? 'selected' : ''; ?>>Pregão Lei Estatais 13.303</option>
+                    <option value="2" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '2') ? 'selected' : ''; ?>>Pregão para Registro de Preço</option>
+                    <option value="18" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '18') ? 'selected' : ''; ?>>RCE-Regime de Contratação Estatal</option>
+                    <option value="9" <?php echo (isset($_POST['modoModalidade']) && $_POST['modoModalidade'] == '9') ? 'selected' : ''; ?>>Regime Diferenciado de Contratação</option>
+
+                  </select>
+                </div>
+                <br />
               </div>
-              <br />
-              <div data-name="2">
+
+
+              <div data-name="2" id="status-group-govC">
                 <div class="form-group">
                   <label for="formGroupExampleInput2">Modalidade</label>
                   <select name="modalidade" id="modalidade" class="form-control fill">
@@ -191,13 +226,27 @@
     document.addEventListener('DOMContentLoaded', function() {
       var fonteSelect = document.querySelector('select[name="fonte"]');
       var statusGroup = document.getElementById('status-group');
+      var statusGroupGovA = document.getElementById('status-group-govA');
+      var statusGroupGovB = document.getElementById('status-group-govB');
+      var statusGroupGovC = document.getElementById('status-group-govC');
 
       function toggleStatus() {
         if (fonteSelect.value === '3') {
           statusGroup.style.display = 'none';
         } else {
-          statusGroup.style.display = '';
+          statusGroup.style.display = 'block';
+
         }
+        if (fonteSelect.value === '2') {
+          statusGroupGovA.style.display = 'none';
+          statusGroupGovB.style.display = 'none';
+          statusGroupGovC.style.display = 'none'
+        } else {
+          statusGroupGovA.style.display = 'block';
+          statusGroupGovB.style.display = 'block';
+          statusGroupGovC.style.display = 'block';
+        }
+
       }
       fonteSelect.addEventListener('change', toggleStatus);
       toggleStatus();
